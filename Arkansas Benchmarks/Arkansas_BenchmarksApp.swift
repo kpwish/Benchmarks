babd68ac14b99122c493afa5b891fd451a3f3717
Kevin Wish <kevinpwish@gmail.com>
2025-12-25 22:21:10 -0600

    Initial Commit

---
+++ Arkansas Benchmarks/Arkansas_BenchmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arkansas_BenchmarksApp.swift
+//  Arkansas Benchmarks
+//
+//  Created by Kevin Wish on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Arkansas_BenchmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
